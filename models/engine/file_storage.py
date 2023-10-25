eee9695b8790d30ef65166a4caaa00b37157a301
Laide Endeley <endeleylaide@gmail.com>
2023-10-25 09:25:42 -0700

    Fixed some pycodestyle errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,8 +57,8 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-        def delete(self, obj=None)
-            """Deletes obj from __objects"""
-            if obj is not None:
-                key = obj.__class__.__name__ + '.' + obj.id
-                del self.__objects[key]
+    def delete(self, obj=None):
+        """Deletes obj from __objects"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            del self.__objects[key]
