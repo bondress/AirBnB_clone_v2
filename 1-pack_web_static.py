9d80b515448dfaff3f54de8ef072f4a643abfb07
Laide Endeley <endeleylaide@gmail.com>
2023-11-09 02:09:34 -0800

    Deploy Static Update: Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# This Fabric script generates a .tgz archive from the contents of web_static.
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    curr_dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(curr_dt.year,
+                                                         curr_dt.month,
+                                                         curr_dt.day,
+                                                         curr_dt.hour,
+                                                         curr_dt.minute,
+                                                         currdt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
